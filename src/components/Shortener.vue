d9942744f2aade05d7710b6313c187f4da7f9f3c
slurdge <slurdge@slurdge.org>
2020-09-26 12:07:46 +0200

    Nicer QRCode card, only activated when there is a shortlink

--- src/components/Shortener.vue
+++ src/components/Shortener.vue
@@ -3,7 +3,11 @@
     <v-toolbar color="primary" dark flat>
       <v-toolbar-title>Shorten link with lc.cx</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-progress-circular indeterminate v-if="working" color="white"></v-progress-circular>
+      <v-progress-circular
+        indeterminate
+        v-if="working"
+        color="white"
+      ></v-progress-circular>
     </v-toolbar>
     <v-card-text>
       <v-form v-model="valid" @keyup.native.enter="shorten()">
@@ -18,16 +22,17 @@
         ></v-text-field>
 
         <v-tooltip bottom v-model="tooltip.show">
-          <template v-slot:activator="{attrs}">
+          <template v-slot:activator="{ attrs }">
             <v-text-field
               id="shortlink"
+              ref="shortlink"
               label="ShortLink"
-              :prepend-icon="shortlink ? 'mdi-qrcode-scan': 'mdi-link'"
+              :prepend-icon="shortlink ? 'mdi-qrcode-scan' : 'mdi-link'"
               type="text"
               v-model="shortlink"
-              :append-icon="shortlink ? 'mdi-content-copy': ''"
+              :append-icon="shortlink ? 'mdi-content-copy' : ''"
               @click:append="copylink"
-              @click:prepend="dialog=true"
+              @click:prepend="shortlink ? (dialog = true) : {}"
               v-bind="attrs"
               readonly
             ></v-text-field>
@@ -37,72 +42,76 @@
         <v-container dense>
           <v-row dense>
             <v-col>
-              <v-switch class="mb-2" v-model="advanced" label="Advanced settings" />
+              <v-switch
+                class="mb-2"
+                v-model="advanced"
+                label="Advanced settings"
+              />
             </v-col>
             <v-col>
               <v-scroll-x-reverse-transition>
-                <v-text-field label="Custom URL" v-model="custom" :rules="customURLRules" v-show="advanced"></v-text-field>
+                <v-text-field
+                  label="Custom URL"
+                  v-model="custom"
+                  :rules="customURLRules"
+                  v-show="advanced"
+                ></v-text-field>
               </v-scroll-x-reverse-transition>
             </v-col>
             <v-col>
               <v-scroll-x-reverse-transition>
-                <v-text-field label="Note" v-model="note" v-show="advanced"></v-text-field>
+                <v-text-field
+                  label="Note"
+                  v-model="note"
+                  v-show="advanced"
+                ></v-text-field>
               </v-scroll-x-reverse-transition>
             </v-col>
           </v-row>
         </v-container>
       </v-form>
-        
-      </v-card-text>
+    </v-card-text>
     <v-card-actions>
-       <v-alert
+      <v-alert
         class="my-auto"
         dense
         transition="fade-transition"
         :value="alert.show"
         :type="alert.type"
-      >{{alert.message}}</v-alert>
+        >{{ alert.message }}</v-alert
+      >
       <v-spacer></v-spacer>
-      <v-btn :disabled="!valid" color="primary" @click="shorten()" large>Shorten</v-btn>
+      <v-btn :disabled="!valid" color="primary" @click="shorten()" large
+        >Shorten</v-btn
+      >
     </v-card-actions>
-    <v-dialog
-      v-model="dialog"
-      max-width="290"
-    >
-      <v-card>
-        <v-card-title class="headline">QR Code</v-card-title>
-
-        <v-card-text>
-          Here is a QR Code that you can scan or copy
-       </v-card-text>
-       <v-container fluid>
-         <v-row justify="center">
-         <qrcode-vue :value="shortlink" size="150" level="M"></qrcode-vue>
-         </v-row>
-       </v-container>
-        <v-card-actions>
+    <v-dialog v-model="dialog" max-width="290">
+      <v-card flat>
+        <v-toolbar color="secondary" dark flat>
+          <v-toolbar-title>QR Code</v-toolbar-title>
           <v-spacer></v-spacer>
-
-          <v-btn
-            text
-            @click="dialog = false"
-          >
-            Close
+          <v-btn icon>
+            <v-icon @click="dialog = false">mdi-close</v-icon>
           </v-btn>
-        </v-card-actions>
+        </v-toolbar>
+        <v-container fluid>
+          <v-row justify="center">
+            <qrcode-vue :value="shortlink" size="150" level="M"></qrcode-vue>
+          </v-row>
+        </v-container>
       </v-card>
     </v-dialog>
   </v-card>
 </template>
 
 <script>
-import QrcodeVue from 'qrcode.vue'
+import QrcodeVue from "qrcode.vue";
 import isURL from "validator/es/lib/isURL";
 
 export default {
   components: {
-      QrcodeVue,
-    },
+    QrcodeVue,
+  },
   props: {
     token: String,
   },
@@ -141,9 +150,7 @@ export default {
             disallow_auth: false,
           }) || "A valid URL is required",
       ],
-      customURLRules: [
-        (v) => /^\S*$/gi.test(v) || "No whitespace is allowed"
-      ]
+      customURLRules: [(v) => /^\S*$/gi.test(v) || "No whitespace is allowed"],
     };
   },
   methods: {
@@ -152,15 +159,21 @@ export default {
         return;
       }
       this.working = true;
-      this.axios.post("/api/shorten", {url: this.link, custom: this.custom, note: this.note}).then((response) => {
-        this.working = false;
-        if (response.data.success) {
-          this.shortlink = response.data.shorturl;
-          this.notify("Done");
-        } else {
-          this.notify("Error: " + response.data.message, "error");
-        }
-      });
+      this.axios
+        .post("/api/shorten", {
+          url: this.link,
+          custom: this.custom,
+          note: this.note,
+        })
+        .then((response) => {
+          this.working = false;
+          if (response.data.success) {
+            this.shortlink = response.data.shorturl;
+            this.notify("Done");
+          } else {
+            this.notify("Error: " + response.data.message, "error");
+          }
+        });
     },
     notify(message, type = "success") {
       this.alert.type = type;
