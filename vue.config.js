d0117016cf9f1ab8cb9869b2668be92c6b1cd931
slurdge <slurdge@slurdge.org>
2022-01-17 23:05:41 +0100

    feat: new versions (wip)

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-    "transpileDependencies": [
-        "vuetify"
-    ],
+
     pages: {
         index: {
             entry: 'src/main.js',
             title: 'URL shortener with lc.cx'
         }
-    }
+    },
+
+    transpileDependencies: [
+      'vuetify'
+    ]
 }
