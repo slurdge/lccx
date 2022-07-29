e884a82b9692f13d4336ec49ae7be41818ba602b
slurdge <slurdge@slurdge.org>
2022-07-29 16:27:40 +0200

    fix: change axios plugin

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(VueAxios, axios)
   .mount('#app')
